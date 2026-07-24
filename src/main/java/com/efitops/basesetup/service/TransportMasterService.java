1c41df9084aca6d11e55feb160dbacb466d541d4
Dhinesh <dhinesh@whydigit.in>
2026-07-24 11:21:41 +0530

    merged ruch code

--- src/main/java/com/efitops/basesetup/service/TransportMasterService.java
+++ src/main/java/com/efitops/basesetup/service/TransportMasterService.java
@@ -15,27 +15,7 @@ import com.efitops.basesetup.exception.ApplicationException;
 
 public interface TransportMasterService {
 
-	Map<String, Object> updateCreateListOfValues(@Valid ListOfValuesDTO dto) throws ApplicationException;
-
-	ListOfValuesVO getListOfValuesById(Long id);
-
-	List<ListOfValuesVO> getListOfValuesByOrgId(Long orgId, Long branchCode);
-
-	Map<String, Object> updateCreateGSTRateMaster(@Valid GSTRateMasterDTO gSTRateMasterDTO) throws ApplicationException;
-
-	GSTRateMasterVO getGSTRateMasterById(Long id) throws ApplicationException;
-
-	List<GSTRateMasterVO> getGSTRateByOrgId(Long orgId, Long branchId) throws ApplicationException;
-
-	Map<String, Object> updateCreateServiceAccMaster(@Valid ServiceAccMasterDTO serviceAccMasterDTO)
-			throws ApplicationException;
-
-	ServiceAccMasterVO getServiceNameById(Long id) throws ApplicationException;
-
-	List<ServiceAccMasterVO> getServiceNameByOrgId(Long orgId, Long branchId) throws ApplicationException;
-
-
-
+	
 	 
 
 

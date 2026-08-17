48783eb6ee6a62104ba3b8755f582f52f2172e89
DiveshWDS <divesh@whydigit.in>
2026-08-17 17:44:55 +0530

    employee issue

--- src/main/java/com/efitops/basesetup/dto/EmployeeResponseDTO.java
+++ src/main/java/com/efitops/basesetup/dto/EmployeeResponseDTO.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 public class EmployeeResponseDTO {
 
 	private Long id;
-	private String employeecode;
-//	private String employeeName;
+//	private String employeecode;
+	private String employeeName;
 
 }

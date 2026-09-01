759002526259aaf9fa32131a24ef98dff100edc5
DiveshWDS <divesh@whydigit.in>
2026-09-01 14:51:45 +0530

    item code bug fix in purchase order amendment

--- src/main/java/com/efitops/basesetup/repository/MachineMasterRepo.java
+++ src/main/java/com/efitops/basesetup/repository/MachineMasterRepo.java
@@ -1,9 +1,14 @@
 package com.efitops.basesetup.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.efitops.basesetup.entity.BranchVO;
 import com.efitops.basesetup.entity.MachineMasterVO;
 
 public interface MachineMasterRepo extends JpaRepository<MachineMasterVO, Long> {
+
+	List<MachineMasterVO> findByOrgIdAndBranch(Long orgId, BranchVO branchVO);
 
 }

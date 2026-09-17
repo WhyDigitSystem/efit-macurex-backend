2e645add20adafd105e0709dc00aa1ddd1ded7e9
narasimhawds <narashima@whydigit.in>
2026-09-17 19:10:32 +0530

    chnages

--- src/main/java/com/efitops/basesetup/entity/MaterialTransferReturnNoteVO.java
+++ src/main/java/com/efitops/basesetup/entity/MaterialTransferReturnNoteVO.java
@@ -113,6 +113,7 @@ public class MaterialTransferReturnNoteVO {
     @Column(name = "screen_code")
     private String screenCode = "MTRN";
 
+    
     @Column(name = "org_id")
     private Long orgId;
 
